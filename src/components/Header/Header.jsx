3c144f68f9f0de4c1b53c7d47279250d00ee705f
Arsen5555 <arsen.xachatryan.20@gmail.com>
2020-04-18 15:17:04 +0400

    new version

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import classes from "./Header.module.css";
 const Header = () => {
   return (
     <header className={classes.header}>
-      <img src="https://i.ebayimg.com/images/g/Q3EAAOSwEp5cHYWi/s-l400.jpg"  alt={}/>
+      <img src="https://i.ebayimg.com/images/g/Q3EAAOSwEp5cHYWi/s-l400.jpg"/>
     </header>
   );
 };
